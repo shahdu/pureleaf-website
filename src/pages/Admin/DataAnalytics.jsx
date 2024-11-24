01c63a043b979323a3da884da95c2dd60f27b070
shahdu <shahadmoalzoman@gmail.com>
2024-11-24 21:42:05 +0300

    fix stilt Analytid

--- src/pages/Admin/DataAnalytics.jsx
+++ src/pages/Admin/DataAnalytics.jsx
@@ -5,7 +5,7 @@ import { AnalytictsOrders } from './AnalytictsOrders';
 
 export const DataAnalytics = () => {
   return (
-    <div style={{ padding: '20px', backgroundColor: '#f9f9f9' }}>
+    <div style={{ padding: '20px'}}>
       <h1 style={{ textAlign: 'center', marginBottom: '20px', color: '#388E3C', fontWeight: 'bold' }}>
         Data Analytics Overview
       </h1>
@@ -15,19 +15,16 @@ export const DataAnalytics = () => {
         
         {/* Analytics Orders Section */}
         <div style={{ flex: '1 1 30%', backgroundColor: '#e8f5e9', padding: '20px', borderRadius: '8px', boxShadow: '0 4px 6px rgba(0,0,0,0.1)' }}>
-          <h2 style={{ color: '#388E3C', textAlign: 'center' }}>Orders Analytics</h2>
           <AnalytictsOrders />
         </div>
         
         {/* Analytics Products Section */}
         <div style={{ flex: '1 1 30%', backgroundColor: '#e8f5e9', padding: '20px', borderRadius: '8px', boxShadow: '0 4px 6px rgba(0,0,0,0.1)' }}>
-          <h2 style={{ color: '#388E3C', textAlign: 'center' }}>Products Analytics</h2>
           <AnalytictsProducts />
         </div>
         
         {/* User Analysis Section */}
         <div style={{ flex: '1 1 30%', backgroundColor: '#e8f5e9', padding: '20px', borderRadius: '8px', boxShadow: '0 4px 6px rgba(0,0,0,0.1)' }}>
-          <h2 style={{ color: '#388E3C', textAlign: 'center' }}>User Analytics</h2>
           <AnalysisUser />
         </div>
         
